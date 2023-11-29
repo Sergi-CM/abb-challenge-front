2d9efe035cc08b669d2e2c55ecd9f3e045a889af
Sergi Casquero <111904117+Sergi-CM@users.noreply.github.com>
2023-11-29 23:20:45 +0100

    Move fetching logic to PartDashboard component

--- src/components/PartDashboard/PartDashboard.tsx
+++ src/components/PartDashboard/PartDashboard.tsx
@@ -1,28 +1,49 @@
-import { PartDashboardStructure } from "../../types/types";
+import { useEffect, useState } from "react";
 import FeatureTable from "../FeatureTable/FeatureTable";
 import styles from "./PartDashboard.module.scss";
+import { PartStructure } from "../../types/types";
 
-interface PartDashboardProps {
-  data: PartDashboardStructure;
-}
+const PartDashboard = () => {
+  const [data, setData] = useState<PartStructure[] | null>(null);
 
-const PartDashboard = ({ data }: PartDashboardProps) => {
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch("http://localhost:4000/parts", {
+          method: "GET",
+          headers: { "Content-Type": "application/json; charset=UTF-8" },
+        });
+
+        const { parts } = await response.json();
+
+        setData(parts);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
   return (
     <section className={styles["part-wrapper"]}>
-      {data.map((part, partIndex) => (
-        <div key={partIndex}>
-          <h1>{part.name}</h1>
-          <div className={styles["tables-wrapper"]}>
-            {part.features.map((feature, featureIndex) => (
-              <FeatureTable
-                featureName={feature.featureName}
-                measurements={feature.measurements}
-                key={featureIndex}
-              />
-            ))}
+      {data ? (
+        data.map((part, partIndex) => (
+          <div key={partIndex}>
+            <h1>{part.name}</h1>
+            <div className={styles["tables-wrapper"]}>
+              {part.features.map((feature, featureIndex) => (
+                <FeatureTable
+                  featureName={feature.featureName}
+                  measurements={feature.measurements}
+                  key={featureIndex}
+                />
+              ))}
+            </div>
           </div>
-        </div>
-      ))}
+        ))
+      ) : (
+        <p>Loading...</p>
+      )}
     </section>
   );
 };
